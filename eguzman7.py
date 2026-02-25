fb9f885ce883f20c80dcabfa62fb6d8e16cd38f2
EG <guzmaneddie99@gmail.com>
2026-02-25 02:18:23 +0000

    Lab #7

---
+++ eguzman7.py
@@ -0,0 +1,25 @@
+#Eduardo Guzman
+
+
+x = "Hello World"
+y = 3
+z = 33.3
+
+
+if x == "Goodbye":
+    print("NICE")
+else:
+    print("Hello World")
+
+if y != 3:
+    print("5 is the magic number!")
+else:
+    print("3 is the magic number!")
+
+if z <= 20.5:
+    print("THATS BETTER")
+else:
+    print("NICE TRY")
+
+
+
