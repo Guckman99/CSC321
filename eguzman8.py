551427c041989166d99dab04948e4b8230fcc959
EG <guzmaneddie99@gmail.com>
2026-03-05 23:35:35 +0000

    lab 8

---
+++ eguzman8.py
@@ -0,0 +1,18 @@
+# Eduardo Guzman
+
+
+secretNum = 7
+userInput = int(input("Guess a number between 0 and 9:"))
+if (userInput < secretNum):
+    print("Too low!")
+elif (userInput > secretNum):
+    print("Too high!")
+else:
+    print("You got it! GG")
+for i in range(5):
+    print("Why don't scientist trust atoms? Because they make up everything! AHAHA")
+count = 0
+while count < 3:
+    print("My favorite food is PIZZA")
+    count += 1
+
